027c6c958183ed9526e215304b5a2d4db69e013f
tamakoshi <tamakosiy@gmail.com>
2023-05-24 21:44:43 +0100

    adapt to CORS and add error handling

--- fetch_diary/main.py
+++ fetch_diary/main.py
@@ -3,20 +3,28 @@ from firebase_admin import firestore
 
 
 def main(request):
+    headers = {
+        'Access-Control-Allow-Origin': '*',  # 許可するオリジンを指定することができます
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',  # 許可するHTTPメソッドを指定します
+        'Access-Control-Allow-Headers': 'Content-Type'  # 許可するヘッダーを指定します
+    }
+
     if (not len(firebase_admin._apps)):
         firebase_admin.initialize_app()
+    try:
+        request = request.get_json(silent=True)
+        date = request['date']
 
-    request = request.get_json(silent=True)
-    date = request['date']
-
-    db = firestore.client()
-    users_ref = db.collection(u'diary').document(date)
-    # ドキュメントのデータを取得
-    doc = users_ref.get()
-    print(doc)
-    if doc.exists:
-        # フィールドの取得
-        fields = doc.to_dict()
-        return fields['text']
-    else:
-        return '400'
+        db = firestore.client()
+        users_ref = db.collection(u'diary').document(date)
+        # ドキュメントのデータを取得
+        doc = users_ref.get()
+        print(doc)
+        if doc.exists:
+            # フィールドの取得
+            fields = doc.to_dict()
+            return (fields['text'], 200, headers)
+        else:
+            return ("日記がありません。", 200, headers)
+    except Exception as e:
+        return (str(e), 500, headers)
--- update_diary/main.py
+++ update_diary/main.py
@@ -4,14 +4,22 @@ from firebase_admin import firestore
 
 @functions_framework.http
 def main(request):
-    request = request.get_json(silent=True)
-    date = request['date']
-    text = request['text']
-    db = firestore.Client()
-    collection_ref = db.collection('diary')
-    doc_ref = collection_ref.document(date)
-    doc_ref.set({
-        'text': text
-    })
-    print('text updated')
-    return '200'
+    headers = {
+        'Access-Control-Allow-Origin': '*',  # 許可するオリジンを指定することができます
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',  # 許可するHTTPメソッドを指定します
+        'Access-Control-Allow-Headers': 'Content-Type'  # 許可するヘッダーを指定します
+    }
+
+    try:
+        request = request.get_json(silent=True)
+        date = request['date']
+        text = request['text']
+        db = firestore.Client()
+        collection_ref = db.collection('diary')
+        doc_ref = collection_ref.document(date)
+        doc_ref.set({
+            'text': text
+        })
+        return ('更新が完了しました', 200, headers)
+    except Exception as e:
+        return (str(e), 500, headers)
